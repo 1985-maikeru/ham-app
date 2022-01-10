8fd2c6545a60e42e5afc738941f2eb33f13984b7
1985-maikeru <blueny1024@gmail.com>
2022-01-10 15:20:56 +0900

    useRefとuseEffect

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 //引数使わない場合、<{}>
 //FC = FunctionComponent 省略できるンゴ！
@@ -6,11 +6,18 @@ const Counter: React.FC<{}> = () => {
   const initialValue: any = 0;
   //useStateに渡す型引数で全てが決まる
   const [value, setValue] = useState<number>(initialValue);
+
+  const renderTimes = useRef<number>(0);
+  useEffect(() => {
+    renderTimes.current = renderTimes.current + 1;
+  });
+
   return (
     <div>
       <div>value:{value}</div>
       <button onClick={() => setValue(value + 1)}>+1</button>
       <button onClick={() => setValue(value - 1)}>-1</button>
+      <div>This component was re-rendered {renderTimes.current} times!</div>
     </div>
   );
 };
