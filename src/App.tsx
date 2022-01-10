4c88b3c26f7e2173ea7fb4fece788185e3ce6a4c
1985-maikeru <blueny1024@gmail.com>
2022-01-10 14:19:15 +0900

    関数コンポーネントの型初級編

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,21 @@ import React from "react";
 import "./App.css";
 
 interface AppProps {
+  //?をつける事で、オプショナルなプロパティにする必要がある
   message?: string;
 }
 
-const App = ({ message }: AppProps) => {
+//React.FunctionComponent　reactが用意している型
+//↑を使う事で、defaultPropsに不正な物を設定した時や違う型の物を渡した時にエラーを出してくれる
+//<>内に型引数を受け取る事ができる
+const App: React.FunctionComponent<AppProps> = ({ message }) => {
   return <p>{message}</p>;
+};
+
+App.defaultProps = {
+  message: "Hello, defaultProps!",
+  //エラーが出る
+  //description: "This is App component.",
 };
 
 export default App;
